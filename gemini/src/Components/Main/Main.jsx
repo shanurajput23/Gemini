09ab7813241ecbf883dc965c77313481ce18626a
shanu <shanurajput7631@gmail.com>
2025-11-05 13:02:33 +0530

    frontend commit

--- gemini/src/Components/Main/Main.jsx
+++ gemini/src/Components/Main/Main.jsx
@@ -34,6 +34,19 @@ function Main() {
                     <img src={assets.code_icon} alt="" />
                 </div>
             </div>
+            <div className="main-bottom">
+                <div className="search-box">
+                    <input type="text" placeholder='Enter a Prompt here' />
+                    <div>
+                        <img src={assets.gallery_icon} alt="" />
+                        <img src={assets.mic_icon} alt="" />
+                        <img src={assets.send_icon} alt="" />
+                    </div>
+                </div>
+                <p className='bottom-info'>
+                    Gemini may display inaccurate info, including about people, so double check the sources.
+                </p>
+            </div>
         </div>
     </div>
   )
